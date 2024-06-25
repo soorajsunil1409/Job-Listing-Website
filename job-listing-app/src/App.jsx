12583b258fd6a37ffac642357c2d0aa8a661b5d2
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-06-25 19:58:31 +0530

    #003
    -> Added a page to read more on a job

--- job-listing-app/src/App.jsx
+++ job-listing-app/src/App.jsx
@@ -5,12 +5,15 @@ import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
 import Page404 from './pages/Page404'
+import SingleJobPage, { jobLoader } from './pages/SingleJobPage'
+import AddJobPage from './pages/AddJobPage'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<MainLayout />}>
     <Route path="/" element={<HomePage />} />
     <Route path="/jobs" element={<JobsPage />} />
-    <Route path="/jobs/:id" element={<SingleJobPage />} />
+    <Route path="/jobs/:id" element={<SingleJobPage />} loader={jobLoader} />
+    <Route path="/add-job" element={<AddJobPage />}/>
     <Route path="*" element={<Page404 />} />
   </Route>
 ))
