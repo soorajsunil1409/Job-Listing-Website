3736b6b69e9ff549e3b35c210b22eae81cc3e144
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-06-27 18:08:42 +0530

    Added asynchronour error handlers

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -1,16 +1,18 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get Jobs
 // @route GET /api/jobs
 // @access Private
-const getJobs = (req, res) => {
+const getJobs = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "Get Jobs" 
     });
-}
+})
 
 // @desc Set Jobs
 // @route POST /api/jobs
 // @access Private
-const postJobs = (req, res) => {
+const postJobs = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
         throw new Error("Please send a text field")
@@ -19,25 +21,25 @@ const postJobs = (req, res) => {
     res.status(201).json({
         message: "Created Jobs"
     })
-}
+})
 
 // @desc Update Job
 // @route PUT /api/jobs/:id
 // @access Private
-const updateJobs = (req, res) => {
+const updateJobs = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Updated Job ${req.params.id}` 
     })
-}
+})
 
 // @desc Get Jobs
 // @route DELETE /api/jobs/:id
 // @access Private
-const deleteJobs = (req, res) => {
+const deleteJobs = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Deleted Job ${req.params.id}` 
     })
-}
+})
 
 module.exports = {
     getJobs,
